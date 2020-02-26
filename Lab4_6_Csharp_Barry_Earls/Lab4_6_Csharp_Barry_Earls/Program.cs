635248e79e7434539d26438a7fbe82c9b3c4642e
bearls <61299567+bearls@users.noreply.github.com>
2020-02-25 19:00:14 -0600

    Add files via upload

---
+++ Lab4_6_Csharp_Barry_Earls/Lab4_6_Csharp_Barry_Earls/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab4_5_Csharp_BarryEarls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+                string[] tasks = new string[7];
+
+                for (int i = 0; i < days.Length; i++)
+                {
+
+                    Console.WriteLine("Type in a new Task for " + days[i]);
+                    tasks[i] = days[i] + " : " + Console.ReadLine();
+
+                }
+
+                for (int i = 0; i < days.Length; i++)
+                {
+                    Console.WriteLine(tasks[i]);
+
+
+
+                }
+            }
+        }
+    }
+}
